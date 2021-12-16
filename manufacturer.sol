2a317149c79174e8b72e0bc361c814febe89b47d
Emin Deniz <emindeniz99@gmail.com>
2021-12-16 16:23:04 +0300

    style: format

--- manufacturer.sol
+++ manufacturer.sol
@@ -5,164 +5,158 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract MyProducttContract is ERC721 {
-        using Counters for Counters.Counter;
+    using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     address manufacturer;
-        address stateContractAddress;//local vm 
+    address stateContractAddress; //local vm
 
     constructor(address _stateContractAddress) ERC721("GameItem", "ITM") {
-        manufacturer=msg.sender;
-stateContractAddress=_stateContractAddress;
+        manufacturer = msg.sender;
+        stateContractAddress = _stateContractAddress;
     }
 
-        struct Owner{
-            address add;
-            bool isVerified;
-        }
+    struct Owner {
+        address add;
+        bool isVerified;
+    }
 
-          event Success(bool success,bool isVerified);
-          event Log(string log);
+    event Success(bool success, bool isVerified);
+    event Log(string log);
 
-    mapping(uint => Owner[]) private prev_owners;
+    mapping(uint256 => Owner[]) private prev_owners;
 
     // hash of serial number and zipcode
-    mapping(uint => uint256) private hashes;
+    mapping(uint256 => uint256) private hashes;
 
-    mapping(uint => address ) public waitingtransfers;
+    mapping(uint256 => address) public waitingtransfers;
 
-//TODO: 
-    mapping(string => uint256 ) public serialNumbers;  // serialnumber -> tokenid
+    //TODO:
+    mapping(string => uint256) public serialNumbers; // serialnumber -> tokenid
+
     // ??? 0 means not minted token, we use 1 for startindex
 
-
-/**
+    /**
     mapping(address => bool ) manufacturers;
 
     function registerFabrika(address fabrika) public {
         manufacturers[fabrika]=true;
     }
 */
-    function mint(string memory serial_number,string memory zipcode) public  {
+    function mint(string memory serial_number, string memory zipcode) public {
         emit Log("gas price:");
 
-        emit Log(string(Strings.toString( tx.gasprice   )));
+        emit Log(string(Strings.toString(tx.gasprice)));
         emit Log("gas current:");
 
-        emit Log(string(Strings.toString( gasleft()   )));
+        emit Log(string(Strings.toString(gasleft())));
 
-
-        require(serialNumbers[serial_number]==0); // indicator for unminted token for the serial no
+        require(serialNumbers[serial_number] == 0); // indicator for unminted token for the serial no
 
         _tokenIds.increment();
 
-                emit Log(zipcode);
+        emit Log(zipcode);
         emit Log(serial_number);
 
         emit Log("aa1");
 
         uint256 newItemId = _tokenIds.current();
-        serialNumbers[serial_number]=newItemId; // serial no
+        serialNumbers[serial_number] = newItemId; // serial no
 
-                emit Log("aa2");
+        emit Log("aa2");
 
         _mint(msg.sender, newItemId);
-                emit Log("aa3");
+        emit Log("aa3");
 
-        hashes[newItemId]=uint256(keccak256( abi.encodePacked(serial_number,zipcode)));
-                emit Log("aa4");
+        hashes[newItemId] = uint256(
+            keccak256(abi.encodePacked(serial_number, zipcode))
+        );
+        emit Log("aa4");
         emit Log(string(Strings.toString(hashes[newItemId])));
         emit Log(string(Strings.toString(newItemId)));
 
-       // require(manufacturers[msg.sender]==true);
-        require(msg.sender==manufacturer);
+        // require(manufacturers[msg.sender]==true);
+        require(msg.sender == manufacturer);
         emit Log("aa5");
 
-
-      prev_owners[newItemId].push(
+        prev_owners[newItemId].push(
             Owner(
                 msg.sender,
                 callQueryVerified(stateContractAddress, msg.sender)
-             )
+            )
         );
         emit Log("aa6");
- emit Log("gas current:");
+        emit Log("gas current:");
 
-        emit Log(string(Strings.toString( gasleft()   )));
-
+        emit Log(string(Strings.toString(gasleft())));
     }
 
-//view olacak mı???
-     function callQueryVerified(address contractaddr, address newOwner) public  returns(bool) { 
+    //view olacak mı???
+    function callQueryVerified(address contractaddr, address newOwner)
+        public
+        returns (bool)
+    {
         //_addr.call{value: msg.value, gas: 5000}(
-            // buradaki memory????
+        // buradaki memory????
 
-        (bool  success, bytes memory data) = contractaddr.call(
+        (bool success, bytes memory data) = contractaddr.call(
             abi.encodeWithSignature("queryVerified(address)", newOwner)
         );
 
-            (bool isVerified) = abi.decode(data, (bool));
-        emit Success(success,isVerified);
-       return  isVerified;
-    }  
-    
-    
+        bool isVerified = abi.decode(data, (bool));
+        emit Success(success, isVerified);
+        return isVerified;
+    }
+
     // to: the address to send the tokenId and the address which is the prospective new owner of the token
     // tokenId: token id of the unique product
     // User can do tokenId transfer by calling this function. If the token is not waiting any user to be accepted by its ownership
     // and if the caller of this function is the last owner of this token then this token is saved in the waitingtransfers mapping.
-    // In this mapping, the key is the tokenId in question and the value is the address that is wanted to send. The ownership of the token 
-    // does not change until the new address accepts this transfer so until that, the tokenId is saved in the waitingtransfers mapping in order 
+    // In this mapping, the key is the tokenId in question and the value is the address that is wanted to send. The ownership of the token
+    // does not change until the new address accepts this transfer so until that, the tokenId is saved in the waitingtransfers mapping in order
     // to prevent token to be sent to others.
-    function transfer(
-        address to,
-        uint256 tokenId
-    ) public virtual { // TODO? virtual ne??
+    function transfer(address to, uint256 tokenId) public virtual {
+        // TODO? virtual ne??
 
-    require(waitingtransfers[tokenId]==address(0));
+        require(waitingtransfers[tokenId] == address(0));
 
-        require(prev_owners[tokenId][prev_owners[tokenId].length-1].add==msg.sender);
+        require(
+            prev_owners[tokenId][prev_owners[tokenId].length - 1].add ==
+                msg.sender
+        );
 
-        waitingtransfers[tokenId]=to;
-        
-
+        waitingtransfers[tokenId] = to;
     }
-        // tokenId: the id of the token whose ownership will be changed via this function
-        // The address of the user who is requested to be the new owner of a token calls this function to approve the transfer and take its ownership.
-        // If the caller of this function is the address waited for the transfer, _transfer function of ERC721.sol library is executed. The new owner is 
-        // pushed into the prevOwner mapping which stores the address of the users and the boolean variable which states them whether they are verified or not.
-        // The new owner and token tuple is deleted from the waitingtransfer mapping. 
-        function approveTransfer(uint256 tokenId) public {
-            require(msg.sender==waitingtransfers[tokenId]);
-        address from=prev_owners[tokenId][prev_owners[tokenId].length-1].add;
+
+    // tokenId: the id of the token whose ownership will be changed via this function
+    // The address of the user who is requested to be the new owner of a token calls this function to approve the transfer and take its ownership.
+    // If the caller of this function is the address waited for the transfer, _transfer function of ERC721.sol library is executed. The new owner is
+    // pushed into the prevOwner mapping which stores the address of the users and the boolean variable which states them whether they are verified or not.
+    // The new owner and token tuple is deleted from the waitingtransfer mapping.
+    function approveTransfer(uint256 tokenId) public {
+        require(msg.sender == waitingtransfers[tokenId]);
+        address from = prev_owners[tokenId][prev_owners[tokenId].length - 1]
+            .add;
         address to = msg.sender;
-                // _transfer function of the ERC721.sol library is executed
-                _transfer(from, to, tokenId);
-            //    require(_checkOnERC721Received(from, to, tokenId, _data), "ERC721: transfer to non ERC721Receiver implementer");
+        // _transfer function of the ERC721.sol library is executed
+        _transfer(from, to, tokenId);
+        //    require(_checkOnERC721Received(from, to, tokenId, _data), "ERC721: transfer to non ERC721Receiver implementer");
 
-                // The address of the new owner is stored in thşs mapping with its verification status. The new owner stored as an Owner object.
-                    prev_owners[tokenId].push(Owner(
-                to,
-                callQueryVerified(stateContractAddress, to)
-             ));
-           // The address is delted from the waiting transfers mapping because the transfer has been executed successfully.
-           delete waitingtransfers[tokenId];
-
-        }
-
-        function trace(uint256 tokenId) public view returns (Owner  [] memory){
-        
-        
-     //   emit Log(string(Strings.toString(prev_owners[tokenId])));
-
-    Owner [] memory temp=new Owner[](prev_owners[tokenId].length);
-    for(uint256 i=0;i<prev_owners[tokenId].length;i++){
-    
-    temp[i]=(prev_owners[tokenId][i]);
+        // The address of the new owner is stored in thşs mapping with its verification status. The new owner stored as an Owner object.
+        prev_owners[tokenId].push(
+            Owner(to, callQueryVerified(stateContractAddress, to))
+        );
+        // The address is delted from the waiting transfers mapping because the transfer has been executed successfully.
+        delete waitingtransfers[tokenId];
     }
+
+    function trace(uint256 tokenId) public view returns (Owner[] memory) {
+        //   emit Log(string(Strings.toString(prev_owners[tokenId])));
+
+        Owner[] memory temp = new Owner[](prev_owners[tokenId].length);
+        for (uint256 i = 0; i < prev_owners[tokenId].length; i++) {
+            temp[i] = (prev_owners[tokenId][i]);
+        }
 
         return temp;
-
-
-        }
-
+    }
 }
--- state.sol
+++ state.sol
@@ -3,32 +3,31 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MyContract is ERC721 {
+    // Stores the address of the owner of the contract
+    address state;
+    // Stores verified addresses in a mapping. If it is verified then it has a value True in this mapping
+    mapping(address => bool) verified;
 
-// Stores the address of the owner of the contract
-address state;
-// Stores verified addresses in a mapping. If it is verified then it has a value True in this mapping
-mapping (address => bool) verified;
-
-// When the contract is deployed, the address of the owner of this contract, who is in another words the chairman of the state, is stored
-// in the -state- variable
-constructor() ERC721("MyNFT", "MNFT") {
-    state=msg.sender;
+    // When the contract is deployed, the address of the owner of this contract, who is in another words the chairman of the state, is stored
+    // in the -state- variable
+    constructor() ERC721("MyNFT", "MNFT") {
+        state = msg.sender;
     }
 
-// When this function is called by the owner of this contract, the manufacturer or the customer address is verified and stored in the -verified- mapping 
-// with the value True
-function verify(address a) external{
-    require(msg.sender==state);
-    verified[a]=true;
-}
-
-// When this function is called, the address given as the parameter is checked whether it has been verified or not. 
-// If it is then function returns true or else false
-function queryVerified(address a) external view returns (bool){
-    if(verified[a]==true){
-        return true;
-    }else{
-        return false;
+    // When this function is called by the owner of this contract, the manufacturer or the customer address is verified and stored in the -verified- mapping
+    // with the value True
+    function verify(address a) external {
+        require(msg.sender == state);
+        verified[a] = true;
     }
-}
+
+    // When this function is called, the address given as the parameter is checked whether it has been verified or not.
+    // If it is then function returns true or else false
+    function queryVerified(address a) external view returns (bool) {
+        if (verified[a] == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
