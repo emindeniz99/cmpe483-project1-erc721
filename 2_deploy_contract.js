99e299b9a50f53b764ca2be17972375d8d93b6cf
Berkant Koç <87160699+berkkant@users.noreply.github.com>
2021-12-18 14:33:12 +0300

    Updated according to changes in ProducttContract.sol

--- 2_deploy_contract.js
+++ 2_deploy_contract.js
@@ -1,12 +1,9 @@
 var state = artifacts.require("state");
-var product = artifacts.require("product");
+var ProducttContract = artifacts.require("ProducttContract");
 
 module.exports = function(deployer){
-  let add = "0x98445ab3eaafdd2293981525631730c64adec41a";
   deployer.deploy(state).then(() => state.deployed())
-  .then(_instance => deployer.deploy(product,_instance.address));
+  .then(_instance => deployer.deploy(ProducttContract,_instance.address));
 
-
-  //deployer.deploy(product,add);
 }
 
